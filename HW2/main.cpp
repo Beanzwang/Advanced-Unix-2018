8b147d65b460d610e8f4d4729b127e31884a7644
Beanzwang <phoebewangintw@gmail.com>
2018-05-11 15:30:55 +0800

    finish indenting & change the data structure using struct.

--- HW2/main.cpp
+++ HW2/main.cpp
@@ -5,25 +5,43 @@
 #include <errno.h>
 #include <vector>
 #include <string>
+#include <string.h>
 #include <sstream>
 #include <fstream>
 #include <unistd.h>
 #include <sys/types.h>
 #include <algorithm>
+#include <iomanip>
 
 #define PS "ps"
 #define EXIT "exit"
 
 uid_t uid;
 gid_t gid;
-int col_index = 0;
+
+struct Row {    
+    pid_t pid;
+    uid_t uid;
+    gid_t gid;
+    pid_t ppid;
+    gid_t pgid;
+    pid_t sid;
+    pid_t tty;
+    char St;
+    char img[100];
+    char cmd[200];
+};
 
 std::vector<std::string> get_pid_dir();
-std::vector<std::vector<std::string> > read_stat(std::vector<std::string> pid_dir);
-bool is_number(const std::string& s);
+std::vector<Row> read_stat(std::vector<std::string> pid_dir);
+bool is_number(const std::string &s);
 void parse_cmd(std::vector<std::string>);
 std::vector<std::string> tokenize(std::string);
-void print_ps(std::vector<std::vector<std::string> > rows, bool show_all);
+void print_ps(std::vector<Row> rows, bool show_all);
+void print_indt(Row row);
+bool sort_ppid(const Row &row1, const Row &row2);
+bool sort_pgid(const Row &row1, const Row &row2);
+bool sort_sid(const Row &row1, const Row &row2);
 
 bool is_number(const std::string &s) {
     // to determine if a string consists of all numbers.
@@ -31,16 +49,17 @@ bool is_number(const std::string &s) {
     return !s.empty() && s.find_first_not_of("0123456789") == std::string::npos;
 }
 
-bool sort_col(const std::vector<std::string> &item1, const std::vector<std::string> &item2) {
-    int int_item1, int_item2;
-    std::istringstream iss1(item1[col_index]);
-    std::istringstream iss2(item2[col_index]);
-    iss1 >> int_item1;
-    iss2 >> int_item2;
-
-    return int_item1 < int_item2;
+bool sort_ppid(const Row &row1, const Row &row2) {
+    return row1.ppid < row2.ppid;
 }
 
+bool sort_pgid(const Row &row1, const Row &row2) {
+    return row1.pgid < row2.pgid;
+}
+
+bool sort_sid(const Row &row1, const Row &row2) {
+    return row1.sid < row2.sid;
+}
 
 std::vector<std::string> tokenize(std::string s) {
     // separate string by spaces
@@ -84,47 +103,44 @@ std::vector<std::string> get_pid_dir() {
     return pid_dir;
 }
 
-std::vector<std::vector<std::string> > read_stat(std::vector<std::string> pid_dir) {
+std::vector<Row> read_stat(std::vector<std::string> pid_dir) {
     // read the stat in pid directory.
     // pid, uid, gid, ppid, pgid, sid, tty, St, (img), cmd
-    std::vector<std::string> row;
-    std::vector<std::vector<std::string> > rows;
+    // std::vector<std::string> row;
+    std::vector<Row> rows;
     std::vector<std::string>::iterator it;
 
     for (it = pid_dir.begin(); it != pid_dir.end(); ++it) {
-        std::ifstream f_stat(*it + std::string("/stat"));
-        std::ifstream f_status(*it + std::string("/status"));
+        FILE *f_stat = fopen((*it + std::string("/stat")).c_str(), "r");
+        FILE *f_status = fopen((*it + std::string("/status")).c_str(), "r");
+        FILE *f_cmd = fopen((*it + std::string("/cmdline")).c_str(), "r");
 
         // separate txt in stat file using white space.
-        std::string stat, status, uid, gid;
-        if (f_stat.is_open() && f_status.is_open()) {
-            std::getline(f_stat, stat);
+        // pid, uid, gid, ppid, pgid, sid, tty, St, (img), cmd
+        Row row;
+        char stat[200], status[200], name[200];
+        
+        if (f_stat == NULL || f_status == NULL || f_cmd == NULL)
+            perror("Error opening file.");
+        
+        std::fgets(stat, 200, f_stat);
+        sscanf(stat, "%d %s %c %d %d %d %d", &row.pid, row.img, &row.St, &row.ppid, &row.pgid, &row.sid, &row.tty);
 
-            for (size_t i = 0; i < 7; ++i) {
-                std::getline(f_status, status);
-            }
-            std::getline(f_status, uid);
-            std::getline(f_status, gid);
-        } else {
-            perror("Cannot open file.");
-        }
-        std::vector<std::string> token_stat = tokenize(stat);
-        std::vector<std::string> token_uid = tokenize(uid);
-        std::vector<std::string> token_gid = tokenize(gid);
+        std::fgets(row.cmd, 200, f_cmd);
 
-        row.push_back(token_stat[0]);
-        row.push_back(token_uid[1]);
-        row.push_back(token_gid[1]);
-        for (size_t i = 3; i < 7; ++i) {
-            // ppid, pgid, sid, tty
-            row.push_back(token_stat[i]);
+        for (size_t i = 0; i < 7; ++i) {
+            std::fgets(status, 200, f_status);
         }
-        row.push_back(token_stat[2]);  // state
-        row.push_back(token_stat[1]);  // img
-        // row.push_back(token_stat[3]);  // cmd??
+        std::fgets(status, 200, f_status);
+        sscanf(status, "%s %d", name, &row.uid);
+        std::fgets(status, 200, f_status);
+        sscanf(status, "%s %d", name, &row.gid);
+
         rows.push_back(row);
-        row.clear();
-        // break;
+
+        fclose(f_stat);
+        fclose(f_status);
+        fclose(f_cmd);
     }
 
     return rows;
@@ -146,15 +162,14 @@ void parse_cmd(std::vector<std::string> tokens) {
         -x: can be used to list processes without an associated terminal.
         */
         std::vector<std::string> pid_dir = get_pid_dir();
-        std::vector<std::vector<std::string> >  rows = read_stat(pid_dir);
+        std::vector<Row> rows = read_stat(pid_dir);
         bool print_all = false;
-        col_index = 0;
 
         if ("" == tokens[1]) {
             // default
-            col_index = 0;
             print_all = false;
         }
+        
         if ("-a" == tokens[1]) {
             print_all = true;
         }
@@ -163,54 +178,50 @@ void parse_cmd(std::vector<std::string> tokens) {
         }
         // -p, -q, -r, and -s, which sort the listed processes by pid (default), ppid, pgid, and sid, respectively.
         if ("-p" == tokens[1]) {
-            col_index = 0;
+            // default, do nothing.
         } else if ("-q" == tokens[1]) {
-            col_index = 3;
+            std::sort(rows.begin(), rows.end(), sort_ppid);
         } else if ("-r" == tokens[1]) {
-            col_index = 4;
+            std::sort(rows.begin(), rows.end(), sort_pgid);
         } else if ("s" == tokens[1]) {
-            col_index = 5;
-        }
-
-        if (col_index != 0) {
-            std::sort(rows.begin(), rows.end(), sort_col);
+            std::sort(rows.begin(), rows.end(), sort_sid);
         }
 
         print_ps(rows, print_all);
     }
 }
 
-void print_ps(std::vector<std::vector<std::string> > rows, bool show_all) {
-    std::vector<std::vector<std::string> >::iterator its;
-    std::vector<std::string>::iterator it;
+void print_ps(std::vector<Row> rows, bool show_all) {
+    std::vector<Row>::iterator it;
 
-    std::cout << "pid\tuid\tgid\tppid\tpgid\tsid\ttty\tSt\t(img)\tcmd" << std::endl;
+    std::cout << std::setw(5) << std::right << "pid" << std::setw(5) << "uid" << std::setw(5) << "gid";
+    std::cout << std::setw(6) << std::right << "ppid" << std::setw(6) << "pgid" << std::setw(6) << "sid";
+    std::cout << std::setw(8) << std::right << "tty";
+    std::cout << " St (img) cmd" << std::endl;
 
-    if(show_all) {
-        for (its = rows.begin(); its != rows.end(); ++its) {
-            for (it = its->begin(); it != its->end(); ++it) {
-                std::cout << (*it) << "\t";
-            }
-            std::cout << std::endl;
+    if (show_all) {
+        for (it = rows.begin(); it != rows.end(); ++it) {
+            print_indt(*it);
         }
     } else {
-        for (its = rows.begin(); its != rows.end(); ++its) {
+        for (it = rows.begin(); it != rows.end(); ++it) {
             // show only the process that the user owns
-            std::vector<std::string> row = (*its);
-            if ((row[1] == std::to_string(uid)) && 
-                (row[2] == std::to_string(gid))) {
-                for (it = its->begin(); it != its->end(); ++it)
-                    std::cout << (*it) << "\t";
-                std::cout << std::endl;
-            } 
+            Row row = (*it);
+            if ((row.uid == uid) && (row.gid == gid)) {
+                print_indt(row);
+            }
         }
     }
 }
 
-int main(int argc, char *argv[]) {
-    setlinebuf(stdin);
-    setvbuf(stdout, NULL, _IONBF, 0);
+void print_indt(Row row) {
+    std::cout << std::setw(5) << std::right << row.pid << std::setw(5) << row.uid << std::setw(5) << row.gid;
+    std::cout << std::setw(6) << std::right << row.ppid << std::setw(6) << row.pgid << std::setw(6) << row.sid;
+    std::cout << std::setw(8) << std::right << row.tty << std::setw(3) << row.St;
+    std::cout << " " << row.img << " " << row.cmd << std::endl;
+}
 
+int main() {
     // get euid & egid of the user
     uid = getuid();
     gid = getgid();
